836a6f5fd8fd8b654e3bbbecc528bb9df0c8233e
RND-HW3 <RND-HW3@RND-HW3>
2025-06-26 15:18:39 +0900

    초기 커밋.

---
+++ dBToRms/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace dBToRms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dBTextbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rmsTextbox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dBTextbox
+            // 
+            this.dBTextbox.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.dBTextbox.Location = new System.Drawing.Point(72, 23);
+            this.dBTextbox.Multiline = true;
+            this.dBTextbox.Name = "dBTextbox";
+            this.dBTextbox.Size = new System.Drawing.Size(64, 24);
+            this.dBTextbox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(30, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "dB:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("굴림", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(12, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 21);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "RMS:";
+            // 
+            // rmsTextbox
+            // 
+            this.rmsTextbox.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.rmsTextbox.Location = new System.Drawing.Point(72, 72);
+            this.rmsTextbox.Multiline = true;
+            this.rmsTextbox.Name = "rmsTextbox";
+            this.rmsTextbox.Size = new System.Drawing.Size(64, 24);
+            this.rmsTextbox.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("굴림", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button1.Location = new System.Drawing.Point(72, 120);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(64, 46);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Cal";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(155, 182);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.rmsTextbox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dBTextbox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox dBTextbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox rmsTextbox;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ dBToRms/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dBToRms
+{    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RmsCal rcal = new RmsCal();            
+            
+            double doubleValue = Double.Parse(dBTextbox.Text);
+
+            rmsTextbox.Text = rcal.DecibellToRms(doubleValue).ToString();            
+        }
+    }
+
+    public class RmsCal
+    {
+        public double step1;
+        public double step2;
+
+        public double DecibellToRms(double decibell)
+        {
+            step1 = decibell / 20;
+            step2 = Math.Pow(10, step1);
+
+            return step2;
+        }
+    }
+}
